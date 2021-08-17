1da7022d5b601809a01cb13289ccc6d6a9a64728
Bryon Shotton <bryon7js@gmail.com>
2021-08-16 20:15:09 -0500

    updated files

--- src/components/deleteButton.js
+++
@@ -1,5 +0,0 @@
-// Your posts should have a way for the active user to delete them, only if isAuthor is true for the post. Go ahead and add that now.
-
-// You'll also have to add a click handler to make that DELETE request. For each post, the delete handler will need a way to recover the post._id to help form the correct URL for the request.
-
-// On successful delete, make sure to remove that post from the page as well as from the array that is holding all posts in state.
--- src/components/searchForm.js
+++
@@ -1,5 +0,0 @@
-// As part of your listings, add a little search form. Listen to the user typing into the field, and filter the listings in your state based on that.
-
-// One thing that is tempting is to replace the state with the results of the filter, but this will mean that if the user deletes the filter, or chooses to filter a new term, that you'd have to re-fetch the original list of postings.
-
-// Instead, keep a searchTerm in your state, and map over the postsToDisplay (or whatever you call it) instead of posts after the filter is applied. Below is a very loose attempt at providing some structure. You'll have to fill in the holes.
