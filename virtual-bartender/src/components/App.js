0c0fdec29b220d2a84ad642cc1766969c46f2562
SystemOfADownyJr <84230714+SystemOfADownyJr@users.noreply.github.com>
2021-08-16 21:04:00 -0500

    Add files via upload

---
+++ virtual-bartender/src/components/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import {
+    Route,
+    Switch
+} from 'react-router-dom';
+
+import Home from './Home';
+import Login from './Login';
+import Header from './Header';
+
+import TokenUtilities from '../utilities/token';
+import Drinks from './Drinks';
+
+import '../style/index.css';
+
+//http://localhost:3000/ -> Home Component
+//http://localhost:3000/login -> Login Component
+const App = () => {
+    const [token, setToken] = useState(TokenUtilities.getToken());
+    const [isLoggedIn, setIsLoggedIn] = useState(!!token);
+
+    useEffect(function() {
+        setIsLoggedIn(!!token);
+    }, [token]);
+
+    return (
+        <>
+            <Header isLoggedIn={isLoggedIn} setToken={setToken} />
+            <main>
+                <Switch>
+                    <Route path="/drinks"><Drinks /></Route>
+                    <Route path="/login"><Login setToken={setToken} /></Route>
+                    <Route path="/"><Home /></Route>
+                </Switch>
+            </main>
+        </>
+    )
+};
+
+export default App;
