6c0c3bd4fe92b79acf87d698831712ea5ac60b53
amsanders99@yahoo.com <62858909+lexiiiiiiii@users.noreply.github.com>
2021-08-22 17:18:17 -0500

    changes

--- src/components/app.js
+++ src/components/app.js
@@ -7,19 +7,30 @@ import Login from './Login';
 import Header from './Header';
 // import addPosts from './AddPosts';
 // import Posts from './Posts';
+import Messages from './messagesForm';
 
 
 getUsers();
 
 const App = () => {
+
+    const [token, setToken] = useState(TokenUtilities.getToken())
+    const [isLoggedIn, setIsLoggedIn] = useState(!!token)
+    //not sure where to define these
+
+    useEffect(function() {
+        setIsLoggedIn(!!token)
+    }, [token])
+
     return (
         <>
-                <Header />
+                <Header isLoggedIn={isLoggedIn} setToken={setToken} />
             <main>
                 <Route path="/" />
                 <Route path="/login"><Login /> </Route>
                 <newPostForm />
                 <postForm />
+                <Route path="/messages"><Messages /> </Route>
                 <searchForm />
             </main>
         </>
---
+++ src/components/deleteButton.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+let isAuthor = true
+const postList = ({post, user}) => {
+    return post.map(post => {
+        Author()
+    })
+}
+function Author() {
+    if (isAuthor) {
+        <button type="button" onClick={() => user(post)}>
+              Delete
+            </button>
+            //append to a div?
+            //recover post._id?
+    }
+}
+
+export default postList
